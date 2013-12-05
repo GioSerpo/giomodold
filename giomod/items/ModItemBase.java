633f39e6dad8f2d9784b1add14f4ba2422c8a032
GioSerpo <gioserpo@gmail.com>
2013-12-04 23:50:09 -0500

    tried to fix issues of .git garbage put into files...

--- giomod/items/ModItemBase.java
+++ giomod/items/ModItemBase.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package giomod.items;
 
 import net.minecraft.client.renderer.texture.IconRegister;
@@ -23,29 +22,3 @@ public class ModItemBase extends Item {
 	}
 
 }
-=======
-package giomod.items;
-
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.item.Item;
-import giomod.GioMod;
-
-public class ModItemBase extends Item {
-	
-	private String textureName;
-	
-	public ModItemBase(int id, String textureName, String unlocName)
-	{
-		super(id);
-		setCreativeTab(GioMod.GioTab);
-		setUnlocalizedName(unlocName);
-		this.textureName = textureName;
-	}
-	
-	public void registerIcons(IconRegister Par1)
-	{
-		itemIcon = Par1.registerIcon("giomod:" + textureName);
-	}
-
-}
->>>>>>> 1e4a46207a31f1402db5e46d4a919f14c3d2c27d
